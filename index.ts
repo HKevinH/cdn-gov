dda81762462d69eedb5940f00980435432799b90
HkevinH <kevinlopez151004@gmail.com>
2024-11-05 03:06:02 -0500

    refactor button component: move Button.tsx to src directory and update import paths

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import "@/styles/global.css";
+import "@styles/global.css";
 
-export { default as Button } from "./components/button/Button";
+export { default as Button } from "./src/components/button/Button";

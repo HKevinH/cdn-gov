98c986adc31fa904126892b71e5922f3834054ac
HkevinH <kevinlopez151004@gmail.com>
2024-11-05 01:57:16 -0500

    enhance Button component: add variant prop for styling options

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -2,10 +2,24 @@ import React, { ButtonHTMLAttributes } from "react";
 
 interface CustomButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
+  variant: "content" | "contour" | "inverted content" | "inverted contour";
 }
 
-const Button: React.FC<CustomButtonProps> = ({ label, ...props }) => {
-  return <button {...props}>{label}</button>;
+const Button: React.FC<CustomButtonProps> = ({ label, variant, ...props }) => {
+  const variantClass = {
+    content: "fill-btn-govco",
+    contour: "outline-btn-govco",
+    "inverted content": "fill-secundary-btn-govco",
+    "inverted contour": "outline-secundary-btn-govco",
+  }[variant];
+
+  const className = `btn-govco ${variantClass}`;
+
+  return (
+    <button className={className} {...props}>
+      {label}
+    </button>
+  );
 };
 
 export default Button;

4b17e84d0a4a830b9507bdffb641384327fc8279
HkevinH <kevinlopez151004@gmail.com>
2024-11-05 02:28:08 -0500

    update package.json and vite.config.ts: add dependencies and configure path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-// vite.config.ts
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
-import { fileURLToPath } from "url";
-
-// Definir __dirname en un entorno de módulos ES
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "./") }],
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, "index.ts"),
       name: "gov-components",
       formats: ["es", "umd"],
-      fileName: (format) => `gov-components.${format}.js`,
+      fileName: (format: any) => `gov-components.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],

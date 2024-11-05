0a02505b1d72146706f5060191b6be5037ea5abe
HkevinH <kevinlopez151004@gmail.com>
2024-11-05 01:51:11 -0500

    add global CSS import and update TypeScript paths for styles

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import "@/styles/global.css";
 
 export { default as Button } from "./components/button/Button";

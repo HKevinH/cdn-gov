79b3a9ebb3a9f079aa354edcff70a212a40368c7
HkevinH <kevinlopez151004@gmail.com>
2024-11-05 03:50:28 -0500

    add SVG icons and update global styles import: include multiple new SVG icons and adjust import path for global styles to use alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
+// vite.config.ts
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { copy } from "vite-plugin-copy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        {
+          src: "src/css",
+          dest: "dist/css",
+        },
+        {
+          src: "src/assets",
+          dest: "dist/assets",
+        },
+      ],
+      hook: "writeBundle",
+    }),
+  ],
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "./src") },
-      { find: "@styles", replacement: path.resolve(__dirname, "src/styles") },
+      { find: "@styles", replacement: path.resolve(__dirname, "css") },
     ],
   },
   build: {

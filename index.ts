b6de22e8fb18dffe9b2abef78baa01904c6427ac
HkevinH <kevinlopez151004@gmail.com>
2024-11-05 04:07:18 -0500

    add main.css import and update package.json files field: include main.css in the index.ts file and adjust the files field in package.json to remove index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import "./css/global.css";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import "./css/main.css";
 
 export { default as Button } from "./src/components/button/Button";

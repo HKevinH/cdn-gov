54d00c6eaa2f5bf021035c9c207a07ee58f39173
HkevinH <kevinlopez151004@gmail.com>
2024-11-05 02:40:08 -0500

    update TypeScript and Vite configuration: change JSX setting and add react/jsx-runtime to external dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -16,11 +16,12 @@ export default defineConfig({
       fileName: (format: any) => `gov-components.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsxRuntime",
         },
       },
     },

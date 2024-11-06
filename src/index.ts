89bf73a508cb22bbcb8bffe640d75282c0ad5113
HkevinH <kevinlopez151004@gmail.com>
2024-11-05 19:48:14 -0500

    refactor exports: enhance component index files by adding re-exports for Button, Modal, Header, and Navbar for improved accessibility

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 export { default as Button } from "./components/button/Button";
 export type { CustomButtonProps } from "./components/button/Button";
+export * from "./components/button";
 
 export { default as Modal } from "./components/modal/Modal";
 export type { ModalProps } from "./components/modal/Modal";
+export * from "./components/modal";
 
 export { default as Header } from "./components/header/Header";
 export type { HeaderProps } from "./components/header/Header";
+export * from "./components/header";
 
 export { default as Navbar } from "./components/navbar/Navbar";
 export type { NavbarProps } from "./components/navbar/Navbar";
+export * from "./components/navbar";

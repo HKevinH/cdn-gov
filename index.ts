57418189f2d8b8a42c4991bd8c174494077bd9f8
HkevinH <kevinlopez151004@gmail.com>
2024-11-05 19:12:01 -0500

    remove unnecessary TypeScript ignore comments; add custom typings reference file

--- index.ts
+++ index.ts
@@ -1,8 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import "./css/global.css";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import "./css/main.css";
 
 export * from "./src/index";

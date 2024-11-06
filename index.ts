069716b465674e5a0cf9a590623bf42aec69b752
HkevinH <kevinlopez151004@gmail.com>
2024-11-05 19:08:12 -0500

    refactor exports: consolidate component exports into a single index file for improved organization

--- index.ts
+++ index.ts
@@ -5,14 +5,4 @@ import "./css/global.css";
 // @ts-ignore
 import "./css/main.css";
 
-export { default as Button } from "./src/components/button/Button";
-export type { CustomButtonProps } from "./src/components/button/Button";
-
-export { default as Modal } from "./src/components/modal/Modal";
-export type { ModalProps } from "./src/components/modal/Modal";
-
-export { default as Header } from "./src/components/header/Header";
-export type { HeaderProps } from "./src/components/header/Header";
-
-export { default as Navbar } from "./src/components/navbar/Navbar";
-export type { NavbarProps } from "./src/components/navbar/Navbar";
+export * from "./src/index";
